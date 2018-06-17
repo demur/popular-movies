7a3b6cae63f49f92c891a938056892d833e787a8
demur <demur2000@mail.ru>
2018-06-16 23:43:21 -0400

    Updating TMDbClient to work with new data utilizing implemented models

--- app/src/main/java/com/udacity/demur/popularmovies/service/TMDbClient.java
+++ app/src/main/java/com/udacity/demur/popularmovies/service/TMDbClient.java
@@ -1,11 +1,14 @@
 package com.udacity.demur.popularmovies.service;
 
 import com.udacity.demur.popularmovies.model.MovieSet;
+import com.udacity.demur.popularmovies.model.ReviewSet;
+import com.udacity.demur.popularmovies.model.VideoSet;
 
 import java.util.Map;
 
 import retrofit2.Call;
 import retrofit2.http.GET;
+import retrofit2.http.Path;
 import retrofit2.http.QueryMap;
 
 public interface TMDbClient {
@@ -32,4 +35,25 @@ public interface TMDbClient {
      */
     @GET("movie/top_rated")
     Call<MovieSet> listTopRatedMovies(@QueryMap Map<String, String> options);
+
+    @GET("movie/{movie_id}/videos")
+    Call<VideoSet> getVideos(@Path("movie_id") int movieId);
+
+    /*
+     * Available options:
+     * String language
+     */
+    @GET("movie/{movie_id}/videos")
+    Call<VideoSet> getVideos(@Path("movie_id") int movieId, @QueryMap Map<String, String> options);
+
+    @GET("movie/{movie_id}/reviews")
+    Call<ReviewSet> getReviews(@Path("movie_id") int movieId);
+
+    /*
+     * Available options:
+     * String language
+     * Integer page
+     */
+    @GET("movie/{movie_id}/reviews")
+    Call<ReviewSet> getReviews(@Path("movie_id") int movieId, @QueryMap Map<String, String> options);
 }
