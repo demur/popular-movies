a7eacd5ab061ddf9474ef3667db09157e5a766fc
demur <demur2000@mail.ru>
2018-06-16 23:51:51 -0400

    Moving auxiliary functions to a separate class

---
+++ app/src/main/java/com/udacity/demur/popularmovies/Utilities.java
@@ -0,0 +1,27 @@
+package com.udacity.demur.popularmovies;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+
+import java.io.ByteArrayOutputStream;
+
+public class Utilities {
+    /*
+     * This function is the product of gar at https://stackoverflow.com/a/4009133
+     * suggested to use by Udacity to implement network connection check
+     * */
+    public static boolean isOnline(Context context) {
+        ConnectivityManager cm =
+                (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo netInfo = cm.getActiveNetworkInfo();
+        return netInfo != null && netInfo.isConnectedOrConnecting();
+    }
+
+    public static byte[] getBitmapAsByteArray(Bitmap bitmap) {
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+        bitmap.compress(Bitmap.CompressFormat.PNG, 100, outputStream);
+        return outputStream.toByteArray();
+    }
+}
